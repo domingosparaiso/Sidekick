1fb34ab52d39b61bf8a0cca5b5cd8121b5d15527
Domingos Paraiso <domingos.paraiso@gmail.com>
2025-04-03 11:11:58 -0300

    Adicionando display.h

---
+++ display.h
@@ -0,0 +1,20 @@
+#ifndef DISPLAY_H
+  #define DISPLAY_H 1
+
+  #define DISPLAY_NONE 0           // Nenhum display
+  #define DISPLAY_SERIAL 1         // Sem display, mas envia mensagens pela serial
+  #define DISPLAY_SSD1306_128X32 2 // OLED [SSD1306] 128x32
+  #define DISPLAY_SSD1306_128X64 3 // OLED [SSD1306] 128x64
+  #define DISPLAY_SSH1106 4        // OLED [SSH1106] 128x64
+  #define DISPLAY_LCD2004 5        // Text LCD [LCD2004] 20x4
+  #define DISPLAY_LCD1602 6        // Text LCD [LCD1602] 16x2
+
+  #define DISPLAY_NAME_NONE "None"
+  #define DISPLAY_NAME_SERIAL "Serial"
+  #define DISPLAY_NAME_SSD1306_128X32 "OLED 128x32"
+  #define DISPLAY_NAME_SSD1306_128X64 "OLED 128x64"
+  #define DISPLAY_NAME_SSH1106 "OLED 128x64"
+  #define DISPLAY_NAME_LCD2004 "Text 20x4"
+  #define DISPLAY_NAME_LCD1602 "Text 16x2"
+
+#endif // <EOF>
